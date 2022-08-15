02af751027d89dbf858b36ed84ebdccf86021c08
MaHryCT3 <mahryct123@gmail.com>
2022-08-15 09:45:00 +0500

    fix: forgot save file before push

--- app/vk_bot/webhook/v1/vk_webhook.py
+++ app/vk_bot/webhook/v1/vk_webhook.py
@@ -2,17 +2,19 @@ from aiohttp import web
 import asyncio
 import os
 
+from loguru import logger
+
 from app import models
 from app.context import AppContext
 from app.vk_bot.events import get_handler
 
 
 async def handle(request: web.Request, ctx: AppContext) -> web.Response:
-    data = models.VKEventData.from_json(await request.json())
     try:
-        handler = get_handler(data)
+        data = models.VKEventData.from_json(await request.json())
     except:
-        pass
+        logger.error(f"Error when trying to get event data {request.text}")
+    handler = get_handler(data)
     if handler is not None:
         asyncio.get_running_loop().create_task(handler(data=data, ctx=ctx))
     return web.Response(text="ok", status=200)
